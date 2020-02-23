36834b040cd2acbf1a42d5e3590291848a9ef305
hamed-fs <hamed@firstsource.io>
2020-02-23 16:17:24 +0330

    - minor refactor

--- lib/json_schema_parser.dart
+++ lib/json_schema_parser.dart
@@ -1,4 +1,5 @@
 import 'package:recase/recase.dart';
+import 'package:dart_style/dart_style.dart';
 import 'package:inflection2/inflection2.dart';
 
 import 'package:flutter_schema_parse/model.dart';
@@ -10,7 +11,17 @@ const Map<String, String> _typeMap = <String, String>{
 };
 
 class JsonSchemaParser {
-  static List<Model> getModel(Map<String, dynamic> jsonSchema) {
+  static String _getObjectName(String type, String name) => type == 'object'
+      ? ReCase(name).pascalCase
+      : type == 'array' ? convertToSingular(ReCase(name).pascalCase) : null;
+
+  static String _getObjectType(String type, String name) => type == 'object'
+      ? ReCase(name).pascalCase
+      : type == 'array'
+          ? 'List<${convertToSingular(ReCase(name).pascalCase)}>'
+          : _typeMap[type];
+
+  static List<Model> _getModel(Map<String, dynamic> jsonSchema) {
     List<Model> parent = [];
 
     if (jsonSchema['properties'] != null) {
@@ -18,14 +29,14 @@ class JsonSchemaParser {
         Model child = Model();
 
         child.title = ReCase(property.key).camelCase;
-        child.type = _getType(property.value['type'], property.key);
-        child.className = _getClassName(property.value['type'], property.key);
+        child.type = _getObjectType(property.value['type'], property.key);
+        child.className = _getObjectName(property.value['type'], property.key);
         child.children = [];
 
         if (property.value['type'] == 'object') {
-          child.children.addAll(getModel(property.value));
+          child.children.addAll(_getModel(property.value));
         } else if (property.value['type'] == 'array') {
-          child.children.addAll(getModel(property.value['items']));
+          child.children.addAll(_getModel(property.value['items']));
         }
 
         parent.add(child);
@@ -35,13 +46,23 @@ class JsonSchemaParser {
     return parent;
   }
 
-  static String _getClassName(String type, String name) => type == 'object'
-      ? ReCase(name).pascalCase
-      : type == 'array' ? convertToSingular(ReCase(name).pascalCase) : null;
+  static String getClass(String className, Map<String, dynamic> schema) {
+    List<Model> models = _getModel(schema);
+    StringBuffer result = StringBuffer();
 
-  static String _getType(String type, String name) => type == 'object'
-      ? ReCase(name).pascalCase
-      : type == 'array'
-          ? 'List<${convertToSingular(ReCase(name).pascalCase)}>'
-          : _typeMap[type];
+    result.write('class $className {');
+    for (Model model in models) {
+      result.write('final ${model.className ?? model.type} ${model.title};');
+    }
+
+    result.write('$className({');
+    for (Model model in models) {
+      result.write('this.${model.title},');
+    }
+    result.write('});');
+
+    result.write('}');
+
+    return DartFormatter().format(result.toString());
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:dart_style/dart_style.dart';
 
-import 'package:flutter_schema_parse/model.dart';
 import 'package:flutter_schema_parse/json_schema_parser.dart';
 
 void main() => runApp(App());
@@ -46,29 +44,9 @@ class _HomePageState extends State<HomePage> {
   void _loadAStudentAsset() async {
     final String json = await rootBundle.loadString('assets/receive.json');
     final Map<String, dynamic> schema = jsonDecode(json);
-    final models = JsonSchemaParser.getModel(schema);
 
-    _printModel('ActiveSymbolResponse', models);
+    print(JsonSchemaParser.getClass('ActiveSymbolResponse', schema));
 
     return;
-  }
-
-  void _printModel(String className, List<Model> models) {
-    StringBuffer result = StringBuffer();
-
-    result.write('class $className {');
-    for (Model model in models) {
-      result.write('final ${model.className ?? model.type} ${model.title};');
-    }
-
-    result.write('$className({');
-    for (Model model in models) {
-      result.write('this.${model.title},');
-    }
-    result.write('});');
-
-    result.write('}');
-
-    print(DartFormatter().format(result.toString()));
   }
 }
